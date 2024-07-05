a46d6420079f3b7c8c87893837cf63e2b2fc2faa
Mohit Jha <95844984+mohitanand744@users.noreply.github.com>
2024-07-05 16:15:16 +0530

    Updated

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 apiUrl = `https://newsapi.org/v2/everything?q=`;
 
+let fetchNews = async (query) => {
+  try {
+    let fetchData = await fetch(
+      `${apiUrl}${query}&apiKey=a4b9eeed786d4868bf252831e3c9dbe9`
+    );
+    let res = await fetchData.json();
+
+    document.getElementById("head").innerText = query;
+    fillCards(res.articles);
+    initPagination();
+  } catch {
+    document.getElementById("head").innerText =
+      "Sorry Our API is not Working..!!!";
+  }
+};
+
+let showData = (query) => {
+  fetchNews(query);
+};
+document.querySelector(".list-container").addEventListener("click", (e) => {
+  const clickedElement = e.target;
+
+  document.querySelectorAll(".list-container .active").forEach((element) => {
+    element.classList.remove("active");
+  });
+
+  clickedElement.classList.add("active");
+});
+
+let searchButton = document.querySelector(".btn");
+searchButton.addEventListener("click", () => {
+  let searchText = document.querySelector(".search-text").value;
+  const query = searchText;
+  if (!query) {
+    document.getElementById("head").innerText = "Please Give Some Input";
+  } else {
+    fetchNews(query);
+  }
+});
+
+window.addEventListener("load", () => {
+  fetchNews("india");
+});
+
 let fillCards = (articles) => {
   let cardContainer = document.getElementById("card-container");
   let template = document.getElementById("template");
@@ -108,47 +152,3 @@ const initPagination = () => {
   controlButtonsStatus();
   setCurrentPage(1);
 };
-
-let fetchNews = async (query) => {
-  try {
-    let fetchData = await fetch(
-      `${apiUrl}${query}&apiKey=a4b9eeed786d4868bf252831e3c9dbe9`
-    );
-    let res = await fetchData.json();
-
-    document.getElementById("head").innerText = query;
-    fillCards(res.articles);
-    initPagination();
-  } catch {
-    document.getElementById("head").innerText =
-      "Sorry Our API is not Working..!!!";
-  }
-};
-
-let showData = (query) => {
-  fetchNews(query);
-};
-document.querySelector(".list-container").addEventListener("click", (e) => {
-  const clickedElement = e.target;
-
-  document.querySelectorAll(".list-container .active").forEach((element) => {
-    element.classList.remove("active");
-  });
-
-  clickedElement.classList.add("active");
-});
-
-let searchButton = document.querySelector(".btn");
-searchButton.addEventListener("click", () => {
-  let searchText = document.querySelector(".search-text").value;
-  const query = searchText;
-  if (!query) {
-    document.getElementById("head").innerText = "Please Give Some Input";
-  } else {
-    fetchNews(query);
-  }
-});
-
-window.addEventListener("load", () => {
-  fetchNews("india");
-});
